3ee2332c45979959fbb31ef8c119bfa2ac5ce8b4
QaimudinKhuwaja <159764940+QaimudinKhuwaja@users.noreply.github.com>
2024-04-15 00:24:11 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/cnv node  
+import inquirer from "inquirer"
+ const currency :any = {
+    USD : 1, //base currency
+    PKR : 277.30, //Pakistan Currency
+    JPY : 153.28, // Japanese currency
+    SAR : 3.75, //Saudi Arabia currency
+    CNY : 7.24, //Chinese currency
+    AFN : 71.18, //Afghanistan currency
+    IQD : 1306.82, //Iraq currency
+    IRR : 42075.00, //Iran currency
+    GBP : 0.80, //Pound currency
+    INR : 83.61, //India currency
+    EUR : 0.94, //Euro currency
+ }
+
+ let Currency = await inquirer.prompt([
+   {
+    name : "from",
+    message : "Enter from Currency",
+    type : "list",
+    choices : ["USD", "PKR","JPY", "SAR","CNY","AFN", "IQD", "IRR", "GBP", "INR", "EUR",]
+  
+   },
+   {
+    name : "to",
+    message : "Enter to Currency",
+    type : "list",
+    choices : ["USD", "PKR","JPY", "SAR","CNY","AFN", "IQD", "IRR", "GBP", "INR", "EUR",]
+   },
+
+{
+name: "amount",
+message: "Enter your amount",
+type: "number",
+}
+
+ ]); 
+ 
+ let fromCurrency = currency[Currency.from];
+ let toCurrency = currency[Currency.to];
+ let amount = Currency.amount;
+ let baseAmount = amount / fromCurrency;
+ let Convert = baseAmount * toCurrency;
+ console.log(Convert);
+ 
